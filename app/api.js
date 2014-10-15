0a2167134c87fcb4fe1d161e0907446800110b8c
Prasanth Vaaheeswaran <prasanth@gmail.com>
2014-10-14 23:56:21 -0400

    Starting API design. 	modified:   app/api.js

--- app/api.js
+++ app/api.js
@@ -1,2 +1,17 @@
 'use strict';
 
+var express = require('express');
+var router  = express.Router();
+
+router.route('/question')
+    .get(function(req,res){
+        res.json({message: "return all questions"});
+    })
+    .post(function(req,res){
+        var title = req.body.title,
+            question = req.body.question,
+            category = req.body.category,
+            author = req.body.author;
+
+        res.json({message: "question created!"});
+    });
